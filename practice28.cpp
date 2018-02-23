9415d6b7c3465bf82547d44cbd8fb73e38f56714
Desperado <424494431@qq.com>
2018-02-23 12:38:26 +0800

    添加练习28的第二种方法

--- practice28.cpp
+++ practice28.cpp
@@ -39,3 +39,65 @@ bool more_than_half_num(int *numbers, int length, int &found_number) {
     return false;
 }
 
+
+int partition(int *numbers, int start, int end) {
+    int p = numbers[start];
+
+    while (start < end) {
+
+        while (start < end && numbers[start] < p) {
+            start++;
+        }
+        while (start < end && numbers[end] > p) {
+            end++;
+        }
+        int temp = numbers[start];
+        numbers[start] = numbers[end];
+        numbers[end] = temp;
+    }
+
+    if (numbers[start] > p) {
+        int temp = numbers[start];
+        numbers[start] = p;
+        p = temp;
+    }
+    return start;
+}
+
+//第一种
+bool more_than_half_nums(int *numbers, int length, int &found_number) {
+    if (numbers == nullptr || length <= 0) {
+        return false;
+    }
+
+    int index = partition(numbers, 0, length - 1);
+    int mid_index = length >> 1;
+
+    int start = 0;
+    int end = length - 1;
+
+    while (index != mid_index) {
+        if (index < mid_index) {
+            start = index + 1;
+            index = partition(numbers, start, end);
+        } else {
+            end = index - 1;
+            index = partition(numbers, start, end);
+        }
+    }
+
+    int r = numbers[mid_index];
+
+    int r_t = 0;
+    for (int i = 0; i < length - 1; ++i) {
+        if (numbers[i] == r) {
+            r_t++;
+        }
+    }
+    if (r_t * 2 >= length) {
+        found_number = r;
+        return true;
+    }
+    return false;
+}
+
